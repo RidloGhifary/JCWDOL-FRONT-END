7b20f3909a38bff061df9be1235358e8362531b7
RidloGhifary <ridloghfry@gmail.com>
2024-06-05 22:08:47 +0700

    learn nextjs

---
+++ 10-ssr&ssg/ssrandssg/src/app/about/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function page() {
+  return (
+    <div>
+      <Link href="/" rel="noopener noreferrer">
+        By Ridlo
+      </Link>
+    </div>
+  );
+}

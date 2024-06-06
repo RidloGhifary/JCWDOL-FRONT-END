99c88a1431e4c6e68a129600e1351fb4bca35b6d
RidloGhifary <ridloghfry@gmail.com>
2024-06-06 18:19:20 +0700

    done with exercise react-redux-toolkit day-09

--- 09-exercise-network-calls/src/components/Register.jsx
+++ 09-exercise-network-calls/src/components/Register.jsx
@@ -1,46 +1,92 @@
-import { useState } from "react";
 import { Form, Button, Container } from "react-bootstrap";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useDispatch } from "react-redux";
+import { addUserData } from "../features/users/usersSlice";
 
 const Register = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // TODO: Implement registration logic
-  };
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .min(6, "is Must be 6 characters or more")
+        .required("is Required"),
+      email: Yup.string()
+        .email("Invalid email address")
+        .required("is Required"),
+      password: Yup.string()
+        .min(6, "Must be 6 characters or more")
+        .matches(/[A-Z]/, "At least contain one uppercase letter")
+        .matches(/[a-z]/, "At least contain one lowercase letter")
+        .matches(/[0-9]/, "At least contain one number")
+        .matches(/[^a-zA-Z0-9]/, "At least contain one special character")
+        .required("is Required"),
+    }),
+    onSubmit: (values, { resetForm }) => {
+      dispatch(addUserData(values));
+      resetForm();
+      alert("Successfully added user");
+    },
+  });
+
   return (
     <Container>
       <h1>Register</h1>
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={formik.handleSubmit}>
         <Form.Group controlId="formBasicName">
-          <Form.Label>Name</Form.Label>
+          <Form.Label>
+            Name{" "}
+            {formik.touched.name && formik.errors.name ? (
+              <span className="text-red-500">{formik.errors.name}</span>
+            ) : null}
+          </Form.Label>
           <Form.Control
             type="text"
             placeholder="Enter name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            name="name"
+            id="name"
+            onChange={formik.handleChange}
+            value={formik.values.name}
           />
         </Form.Group>
 
         <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
+          <Form.Label>
+            Email address{" "}
+            {formik.touched.email && formik.errors.email ? (
+              <span className="text-rose-500">{formik.errors.email}</span>
+            ) : null}
+          </Form.Label>
           <Form.Control
             type="email"
             placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+            id="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
           />
         </Form.Group>
 
         <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
+          <Form.Label>
+            Password{" "}
+            {formik.touched.password && formik.errors.password ? (
+              <span className="text-rose-500">{formik.errors.password}</span>
+            ) : null}
+          </Form.Label>
           <Form.Control
             type="password"
             placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            name="password"
+            id="password"
+            onChange={formik.handleChange}
+            value={formik.values.password}
           />
         </Form.Group>
 
--- 09-exercise-network-calls/src/features/users/usersSlice.js
+++ 09-exercise-network-calls/src/features/users/usersSlice.js
@@ -11,9 +11,19 @@ export const fetchUserRequest = createAsyncThunk(
   "users/fetchUserRequest",
   async (_, { rejectWithValue }) => {
     try {
-      const { data } = await axios.get(
-        `https://jsonplaceholder.typicode.com/users`
-      );
+      const { data } = await axios.get(`http://localhost:3000/users`);
+      return data;
+    } catch (err) {
+      return rejectWithValue(err.message);
+    }
+  }
+);
+
+export const addUserData = createAsyncThunk(
+  "users/addUserData",
+  async (user, { rejectWithValue }) => {
+    try {
+      const { data } = await axios.post("http://localhost:3000/users", user);
       return data;
     } catch (err) {
       return rejectWithValue(err.message);
@@ -35,6 +45,19 @@ const reducer = createSlice({
         state.users = action.payload;
       })
       .addCase(fetchUserRequest.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      // HANDLING ADD USER
+      .addCase(addUserData.pending, (state) => {
+        state.loading = true;
+        state.error = "";
+      })
+      .addCase(addUserData.fulfilled, (state, actions) => {
+        state.loading = false;
+        state.users.push(actions.payload);
+      })
+      .addCase(addUserData.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
       });

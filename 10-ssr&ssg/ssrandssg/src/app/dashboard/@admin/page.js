12793c0c2b2ae3bf1bb2e6d20820f6e8209e9075
RidloGhifary <ridloghfry@gmail.com>
2024-06-08 18:53:46 +0700

    codeium testing

--- 10-ssr&ssg/ssrandssg/src/app/dashboard/@admin/page.js
+++ 10-ssr&ssg/ssrandssg/src/app/dashboard/@admin/page.js
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default async function Page() {
   await new Promise((resolve) => setTimeout(resolve, 5000));
+
   return (
     <div>
       <p>Admin dashboard</p>
+      <Link href="/">Return Home</Link>
     </div>
   );
 }

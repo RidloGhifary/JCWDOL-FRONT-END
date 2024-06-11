1ca603fa8a598ced9fecaab3e1e6c75a1d544ca4
RidloGhifary <ridloghfry@gmail.com>
2024-06-11 21:54:13 +0700

    learn strapi & done with exercise cms

--- 12-headless-cms/src/app/page.js
+++ 12-headless-cms/src/app/page.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen p-24">
+    <main className="min-h-screen p-24">
       <h1>Fetch all entries</h1>
+      <Link href="/testimonial">Testimonial</Link>
     </main>
   );
 }
--- 12-headless-cms/src/components/TestimonialCard.jsx
+++ 12-headless-cms/src/components/TestimonialCard.jsx
@@ -3,22 +3,22 @@ import Image from "next/image";
 export default function TestimonialCard({ entry }) {
   return (
     <>
-      <div className="flex items-start gap-2 bg-gray-900 hover:scale-105 transition cursor-pointer p-5 rounded-md">
+      <div className="flex items-start gap-3 bg-gray-900 hover:scale-105 transition cursor-pointer p-5 rounded-md">
         <Image
           src={`https:${entry.imageUrl}`}
           alt={entry.username}
           width={90}
           height={90}
           loading="lazy"
-          className="aspect-square w-12 h-12 rounded-full object-cover"
+          className="aspect-square w-16 h-1w-16 rounded-full object-cover"
         />
-        <div className="space-y-1">
-          <h1>{entry.username}</h1>
-          <p className="text-gray-400 text-sm">
+        <div className="space-y-2">
+          <h1 className="text-xl font-semibold">{entry.username}</h1>
+          <time className="text-gray-400 text-base italic">
             {new Date(entry.createdAt).getMonth()} -{" "}
             {new Date(entry.createdAt).getFullYear()}
-          </p>
-          <p className="text-gray-400 line-clamp-3">{entry.comment}</p>
+          </time>
+          <p className="text-gray-400 line-clamp-3 text-lg">{entry.comment}</p>
         </div>
       </div>
     </>
